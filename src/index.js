0492c5311100f86cb44eb40937d00323774d0f94
Tomas Pando <tomaspandom@gmail.com>
2023-11-28 17:22:17 -0300

    Actualizacion Mongoose

--- src/index.js
+++ src/index.js
@@ -12,23 +12,9 @@ const app = express()
 const PORT = 8080
 const MONGOOSE_URL = "mongodb+srv://tomas_pando:poker1994@coder.wds0shg.mongodb.net/?retryWrites=true&w=majority"
 
-/* const httpServer = app.listen(PORT, () => {
-    console.log("Servidor activo")
-})
-const socketServer = new Server(httpServer)
-socketServer.on("connection", socket => {
-    console.log(socket.id)
 
-    socket.on("message", data => {
-        console.log(data)
-        socket.emit("confirmation", "Esta es la confirmación")
-    }) //Aca el SERVIDOR escucha y luego con el Emit, emite. 
 
-    socket.on("new_message", data => {
-        socketServer.emit("message_added", data)
-    })
-})
- */
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
@@ -44,9 +30,25 @@ app.use("/static", express.static(`${__dirname}/public`))
 
 try {
     await mongoose.connect(MONGOOSE_URL)
-    app.listen(PORT, () => {
-        console.log("Backend activo y conectado a BBDD")
+    const httpServer = app.listen(PORT, () => {
+        console.log("Servidor activo y conectado a BBDD")
     })
+    const socketServer = new Server(httpServer)
+    socketServer.on("connection", socket => {
+        console.log(socket.id)
+
+        socket.on("message", data => {
+            console.log(data)
+            socket.emit("confirmation", "Esta es la confirmación")
+        }) //Aca el SERVIDOR escucha y luego con el Emit, emite. 
+
+        socket.on("new_message", data => {
+        socketServer.emit("message_added", data)
+        })
+    })
+    /* app.listen(PORT, () => {
+        console.log("Backend activo y conectado a BBDD")
+    }) */
 } catch (error) {
     console.log("No se puede conectar con bbdd", error.message)
 }
